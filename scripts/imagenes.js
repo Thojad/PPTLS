18cef216215dcd0be24be2bd1069af6955147be2
Clinton Villanueva <clinton.a.villanueva@gmail.com>
2015-09-13 17:12:43 -0500

    vincule las imagenes al figure .usuario con javascript inneHTML

---
+++ scripts/imagenes.js
@@ -0,0 +1,5 @@
+function insertar(numero) {
+	var nameImage = ["rock","paper","scissors","lizard","spock"]
+	var resultado = document.getElementById('user').innerHTML = "<img class=\"pos_user\" src=\"img/"+nameImage[numero]+".jpg\"/>";
+}
+	
