7652c044dfb83cff03b9f0a1af9cf2b9ead8a5dd
Clinton Villanueva <clinton.a.villanueva@gmail.com>
2015-09-13 22:37:25 -0500

    añadi algoritmo a la funcion insertar() para relacionar por medio de un input type=text a la funcion juego

--- scripts/spock.js
+++ scripts/spock.js
@@ -1,12 +1,34 @@
-function insertar(numero) {
-	var nameImage = ["rock","paper","scissors","lizard","spock"]
-	var resultado = document.getElementById('user').innerHTML = "<img class=\"pos_user\" src=\"img/"+nameImage[numero]+".jpg\"/>";
-	return numero;
+function insertar(pos) {
+	//inserta una imagen relacionando cada item del menu.
+	var nameImage = ["rock.jpg","paper.jpg","scissors.jpg","lizard.jpg","spock.jpg"]
+	var resultado = document.getElementById('user').innerHTML = "<img class=\"pos_user\" src=\"img/"+nameImage[pos]+"\"/>";
+
+	var rock = "<img class=\"pos_user\" src=\"img/"+nameImage[0]+"\"/>";
+	var paper = "<img class=\"pos_user\" src=\"img/"+nameImage[1]+"\"/>";
+	var scissors = "<img class=\"pos_user\" src=\"img/"+nameImage[2]+"\"/>";
+	var lizard = "<img class=\"pos_user\" src=\"img/"+nameImage[3]+"\"/>";
+	var spock = "<img class=\"pos_user\" src=\"img/"+nameImage[4]+"\"/>";
+	//condiciones para devolver un valor por medio de un input type="text"
+	//el valor retornado en input se usa en opcionUsuario.
+	if (resultado == rock){
+		document.getElementById('user').innerHTML += "<input type=\"text\" id=\"dato\" value=\"0\"/>";
+	}else if(resultado == paper){
+		document.getElementById('user').innerHTML += "<input type=\"text\" id=\"dato\" value=\"1\"/>";
+	}else if(resultado == scissors){
+		document.getElementById('user').innerHTML += "<input type=\"text\" id=\"dato\" value=\"2\"/>";
+	}else if(resultado == lizard){
+		document.getElementById('user').innerHTML += "<input type=\"text\" id=\"dato\" value=\"3\"/>";
+	}else{
+		document.getElementById('user').innerHTML += "<input type=\"text\" id=\"dato\" value=\"4\"/>";
+	}
+	
 }
 
-function juego () {
 
+function juego () {
 //Genera un numero aleatorio entre un rango de enteros
+
+
 function aleatorio (minimo,maximo) {
 	var numero = Math.floor(Math.random()*(maximo - minimo +1) + minimo);
 	return numero;
@@ -21,17 +43,15 @@ var spock = 4;
 
 var opciones = ["Piedra", "Papel", "Tijera", "Lagarto","Spock"];
 
-var opcionUsuario;
+var opcionUsuario = document.getElementById('dato').value;
 var opcionMaquina = aleatorio(0,4);;
 
-/*opcionUsuario = document.getElementById("user").value;*/
+insertarEnMaquina(opcionMaquina);
 
-alert(insertar());
-
-/*document.getElementById('principal').innerHTML += "Elegiste " + opciones[opcionUsuario];
-document.getElementById('principal').innerHTML += "<br>JavaScript eligió " + opciones[opcionMaquina];*/
-
-
+function insertarEnMaquina(pos){
+	var nameImage = ["rock2.jpg","paper.jpg","scissors.jpg","lizard2.jpg","spock.jpg"]
+	var resultado = document.getElementById('machine').innerHTML = "<img class=\"pos_user\" src=\"img/"+nameImage[pos]+"\"/>";
+}
 
 if (opcionUsuario == piedra) {
 	if (opcionMaquina == piedra) {
