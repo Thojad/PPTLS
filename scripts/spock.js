85f958585f93795faf1221d6157c746aa5b6904e
Clinton Villanueva <clinton.a.villanueva@gmail.com>
2016-02-01 18:25:47 -0500

    Las imagenes se han dimensionado correctamente y se han cambiado a png

--- scripts/spock.js
+++ scripts/spock.js
@@ -6,12 +6,12 @@ function aleatorio (minimo,maximo) {
 
 function imagenes(jugador,nombre){
 	if(jugador == 'usuario'){
-		var elemento = document.getElementById(jugador).innerHTML = "<img src=\"img/" + nombre + ".jpg\" class=\"center-block img-responsive\">";
+		var elemento = document.getElementById(jugador).innerHTML = "<img src=\"img/" + nombre + ".png\" class=\"center-block img-responsive\">";
 	}
 	else{
-		elemento = document.getElementById(jugador).innerHTML = "<img src=\"img/" + nombre + ".jpg\" class=\"center-block img-responsive\">";
+		elemento = document.getElementById(jugador).innerHTML = "<img src=\"img/" + nombre + ".png\" class=\"center-block img-responsive\">";
 	}
-	
+
 	return elemento;
 }
 
@@ -36,7 +36,7 @@ function desaparecer(bloque){
 		document.getElementById('Papel').disabled = false;
 		document.getElementById('Tijera').disabled = false;
 		document.getElementById('Lagarto').disabled = false;
-		document.getElementById('Spock').disabled = false;	
+		document.getElementById('Spock').disabled = false;
 	}
 
 }
@@ -60,16 +60,16 @@ function juego(valor){
 
 	var opcionMaquina = opciones[aleatorio(0,4)];
 
-	
+
 	var condiciones = [opcionUsuario == 'Piedra' && opcionMaquina == 'Tijera',
-						opcionUsuario == 'Piedra' && opcionMaquina == 'Lagarto', 
-						opcionUsuario == 'Papel' && opcionMaquina == 'Piedra', 
-						opcionUsuario == 'Papel' && opcionMaquina == 'Spock', 
-						opcionUsuario == 'Tijera' && opcionMaquina == 'Papel', 
-						opcionUsuario == 'Tijera' && opcionMaquina == 'Lagarto', 
-						opcionUsuario == 'Lagarto' && opcionMaquina == 'Papel', 
-						opcionUsuario == 'Lagarto' && opcionMaquina == 'Spock', 
-						opcionUsuario == 'Spock' && opcionMaquina == 'Piedra', 
+						opcionUsuario == 'Piedra' && opcionMaquina == 'Lagarto',
+						opcionUsuario == 'Papel' && opcionMaquina == 'Piedra',
+						opcionUsuario == 'Papel' && opcionMaquina == 'Spock',
+						opcionUsuario == 'Tijera' && opcionMaquina == 'Papel',
+						opcionUsuario == 'Tijera' && opcionMaquina == 'Lagarto',
+						opcionUsuario == 'Lagarto' && opcionMaquina == 'Papel',
+						opcionUsuario == 'Lagarto' && opcionMaquina == 'Spock',
+						opcionUsuario == 'Spock' && opcionMaquina == 'Piedra',
 						opcionUsuario == 'Spock' && opcionMaquina == 'Tijera'];
 
 	if (opcionUsuario == opcionMaquina) {
@@ -84,7 +84,7 @@ function juego(valor){
 	else if (condiciones[0] || condiciones[1] || condiciones[2] || condiciones[3] || condiciones[4] || condiciones[5] || condiciones[6] || condiciones[7] || condiciones[8] || condiciones[9]){
 		imagenes('usuario',opcionUsuario);
 		imagenes('maquina',opcionMaquina);
-		document.getElementById('usuario').style.backgroundColor = "rgba(92,184,92,0.6)"; /*cambia a verde #5CB85C*/ 
+		document.getElementById('usuario').style.backgroundColor = "rgba(92,184,92,0.6)"; /*cambia a verde #5CB85C*/
 		document.getElementById('maquina').style.backgroundColor = "rgba(217,83,92,0.6)"; /*cambia a rojo #D9534F*/
 		document.getElementById('mensaje').style.color = '#5CB85C';
 		resultado('You Win!');
@@ -101,5 +101,3 @@ function juego(valor){
 	limiteClick(valor);
 
 }
-
-
